cfa59518b4c7bda254f15dcdac8437bbd336f5bc
Laken Harville <rlakenharville@gmail.com>
2022-04-28 11:03:52 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PracticeLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var games = new List<string>() { "Call of Duty", "IT", "Forza Horizon", "Star Wars", "Ghost Recon: Wildlands" };
+            var lengths = games.OrderBy(game => game.Length).ToList();
+            lengths.ForEach(x => Console.WriteLine(x));
+           
+         }
+    }
+}
